a061a5fa0c546a937ec191b9fdd08fe395ce8abb
yordan <yordan@infinitelambda.com>
2022-03-23 15:55:24 +0200

    Created customer entity for postgres DB

--- customer/src/main/java/com/yordanm/customer/models/Customer.java
+++ customer/src/main/java/com/yordanm/customer/models/Customer.java
@@ -1,13 +1,27 @@
 package com.yordanm.customer.models;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
 
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE
+    )
     private int id;
     private String firstName;
     private String lastName;
