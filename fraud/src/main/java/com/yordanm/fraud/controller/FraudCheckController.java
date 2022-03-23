64b6c8db054367d258e81f4ceaa83841203af9aa
yordan <yordan@infinitelambda.com>
2022-03-23 20:42:25 +0200

    Initial commit

--- fraud/src/main/java/com/yordanm/fraud/controller/FraudCheckController.java
+++ fraud/src/main/java/com/yordanm/fraud/controller/FraudCheckController.java
@@ -3,11 +3,10 @@ package com.yordanm.fraud.controller;
 import com.yordanm.fraud.model.FraudCheckResponse;
 import com.yordanm.fraud.service.FraudCheckService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/fraud-check")
 @RequiredArgsConstructor
@@ -15,10 +14,10 @@ public class FraudCheckController {
 
     private final FraudCheckService fraudCheckService;
 
-    @PostMapping(path = "{customerId}")
+    @GetMapping(path = "/{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable Integer customerId){
         final boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
-
+        log.info("Customer with id: {} is been checked.", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
