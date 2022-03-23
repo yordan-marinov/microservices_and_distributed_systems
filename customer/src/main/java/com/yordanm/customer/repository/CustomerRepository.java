a8ee97887bada77b99a45faa4c114b4c90d7d9d2
yordan <yordan@infinitelambda.com>
2022-03-23 20:45:41 +0200

    Renamed from crud to jpa repository

--- customer/src/main/java/com/yordanm/customer/repository/CustomerRepository.java
+++ customer/src/main/java/com/yordanm/customer/repository/CustomerRepository.java
@@ -1,9 +1,9 @@
 package com.yordanm.customer.repository;
 
 import com.yordanm.customer.models.Customer;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
