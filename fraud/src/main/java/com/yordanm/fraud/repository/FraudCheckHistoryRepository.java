6ba7e5083715e9513468c25d2452aaf7d32140a3
yordan-marinov <jordanmarinov8@gmail.com>
2022-07-09 19:36:16 +0300

    Added postgres on docker

--- fraud/src/main/java/com/yordanm/fraud/repository/FraudCheckHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yordanm.fraud.repository;
-
-import com.yordanm.fraud.model.FraudCheckHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory, Integer> {
-}
