d289d78991a265d200ea388ef670828dd4988368
yordan-marinov <jordanmarinov8@gmail.com>
2022-07-09 20:34:11 +0300

    Added Fraud service

--- customer-svc/src/main/java/com/yordanm/customer/controller/CustomerController.java
+++ customer-svc/src/main/java/com/yordanm/customer/controller/CustomerController.java
@@ -4,17 +4,21 @@ import com.yordanm.customer.model.Customer;
 import com.yordanm.customer.service.CustomerService;
 import com.yordanm.customer.model.CustomerRegistrationRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("api/v1/customers")
+@RequestMapping("/api/v1/customers")
 public record CustomerController(CustomerService customerService) {
 
-    public Customer registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
+    @PostMapping
+    public ResponseEntity<Customer> registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("New customer registration {}", customerRegistrationRequest);
-        return customerService.register(customerRegistrationRequest);
+        return ResponseEntity.ok(customerService.register(customerRegistrationRequest));
     }
 }
--- customer-svc/src/main/java/com/yordanm/customer/service/CustomerService.java
+++ customer-svc/src/main/java/com/yordanm/customer/service/CustomerService.java
@@ -2,17 +2,24 @@ package com.yordanm.customer.service;
 
 import com.yordanm.customer.model.Customer;
 import com.yordanm.customer.model.CustomerRegistrationRequest;
+import com.yordanm.customer.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomerService {
 
-    public Customer register(CustomerRegistrationRequest customerRegistrationRequest) {
+    private final CustomerRepository customerRepository;
 
-        return Customer.builder()
+    public Customer register(CustomerRegistrationRequest customerRegistrationRequest) {
+        Customer customer =  Customer.builder()
                 .firstName(customerRegistrationRequest.firstName())
                 .lastName(customerRegistrationRequest.lastName())
                 .email(customerRegistrationRequest.email())
                 .build();
+
+        customerRepository.save(customer);
+        return customer;
     }
 }
