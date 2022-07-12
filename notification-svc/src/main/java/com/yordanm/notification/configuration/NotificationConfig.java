5ce203f190fe3e9410e7427c24218e1f28d94781
yordan-marinov <jordanmarinov8@gmail.com>
2022-07-12 21:00:14 +0300

    Set the confing for rabbitmq

--- notification-svc/src/main/java/com/yordanm/notification/configuration/NotificationConfig.java
+++ notification-svc/src/main/java/com/yordanm/notification/configuration/NotificationConfig.java
@@ -1,7 +1,12 @@
 package com.yordanm.notification.configuration;
 
 import lombok.Getter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,8 +17,25 @@ public class NotificationConfig {
     private String internalExchange;
 
     @Value("${rabbitmq.queue.notification}")
-    private String notification;
+    private String notificationQueue;
 
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKey;
+
+    @Bean
+    public TopicExchange internalTopicExchange() {
+        return new TopicExchange(this.internalExchange);
+    }
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(this.notificationQueue);
+    }
+
+    @Bean
+    public Binding internalToNotificationBinding() {
+        return BindingBuilder.bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(this.internalNotificationRoutingKey);
+    }
 }
