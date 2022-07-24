06c2bf90177ab79b9b528cfdf1bff713a0a5acf6
yordan-marinov <jordanmarinov8@gmail.com>
2022-07-24 13:27:54 +0300

    Refactoring

---
+++ docker_postgres_init.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE customer_db
+    WITH
+    OWNER = d_user
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE fraud_db
+    WITH
+    OWNER = d_user
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE notification_db
+    WITH
+    OWNER = d_user
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
